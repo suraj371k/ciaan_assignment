a1651fc9e476026af744ab2543b3bfdc8bf69ef5
suraj371k <surajkushwaha371@gmail.com>
2025-08-04 01:02:00 +0530

    adding frontend url

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  origin: 'https://ciaan-assignment-xi.vercel.app',
   credentials: true
 }));
 app.use(express.json());
